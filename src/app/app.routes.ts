878d2b45fc9a690148dbb96074ded80839720b58
abalinovac <aleksandra.balinovac@gmail.com>
2026-03-15 19:41:41 +0100

    home page initial upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
-import { Details } from './details/details';
 import { Login } from './login/login';
 import { Singup } from './singup/singup';
+import { Movie } from './movie/movie';
 
 export const routes: Routes = [
     {path: '', component: Home, title: 'Home' },
     {path: 'about', component: About, title: 'About'},
-    {path: 'details/:id', component: Details, title: 'Details'},
+    {path: 'movie/:path', component: Movie, title: 'Movie'},
     {path: 'login', component: Login, title: 'Login'},
     {path: 'singup', component: Singup, title: 'Singup'},
     {path: '**', redirectTo: '' }
