77f7cd94da283e950cfd75ec6a7e1b093a291adf
abalinovac <aleksandra.balinovac@gmail.com>
2026-03-17 21:11:02 +0100

    chatbot initial upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,17 +3,71 @@ import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/rou
 import { UserService } from '../services/user.service';
 import { Utils } from './utils';
 import Swal from 'sweetalert2';
+import { MessageModel } from '../models/message.model';
+import { RasaService } from '../services/rasa.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected year = new Date().getFullYear()
+  protected waitingForResponse: boolean = false
+  protected botThinkingPlaceholder: string = 'Thinking...'
+  protected isChatVisible: boolean = false
+  protected userMessage: string = ''
+  protected messages: MessageModel[] = []
+  msg: any;
 
-  constructor(private router: Router, private utils: Utils) { }
+  constructor(private router: Router, private utils: Utils) {
+    this.messages.push({
+      type: 'bot',
+      text: 'How can I help you..?'
+    })
+
+  }
+
+  toggleChat() {
+    this.isChatVisible = !this.isChatVisible
+  }
+
+  sendUserMessage() {
+    if (this.waitingForResponse) return
+
+    const trimmedMessage = this.userMessage.trim()
+    this.userMessage = ''
+
+    this.messages.push({
+      type: 'user',
+      text: trimmedMessage
+    })
+
+    this.messages.push({
+      type: 'bot',
+      text: this.botThinkingPlaceholder
+    })
+
+    RasaService.sendMessage(trimmedMessage).then(rsp => {
+      if (rsp.data.length == 0) {
+        this.messages.push({
+          type: 'bot',
+          text: 'Sorry, I didnt undrestand that.'
+        })
+
+        return
+      }
+
+      for (let botMessage of rsp.data)
+        this.messages.push({
+          type: 'bot',
+          text: botMessage.text
+        })
+    })
+
+  }
 
   getUserName() {
     const user = UserService.getActiveUser()
@@ -27,11 +81,11 @@ export class App {
   doLogout() {
     this.utils.showDialog(
       "Are you sure you want to logout?", () => {
-      UserService.logout()
-      this.router.navigateByUrl('/login')
-    },
-    "Logout now",
-    "Don't Logout"
-  )
+        UserService.logout()
+        this.router.navigateByUrl('/login')
+      },
+      "Logout now",
+      "Don't Logout"
+    )
   }
 }
